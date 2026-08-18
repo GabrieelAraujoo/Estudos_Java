5e1637917bf2eb17cc963ca6e779ce16031940cd
Gabriel Araújo Santos <135062775+GabrieelAraujoo@users.noreply.github.com>
2026-08-18 15:21:04 -0300

    Add files via upload

---
+++ aula.java
@@ -0,0 +1,12 @@
+public class aula {
+    public static void main(String[] args) {
+       // System.out.println("Olá mundo");
+        String nome = "maria";
+        int idade = 30;
+        double renda = 4000.0;
+
+        System.out.printf("%s tem %d anos e ganha R$ %.2f%n", nome,idade,renda);
+        
+    }
+}
+
